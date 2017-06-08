47a95e22491342e3116c8e4af38aacf40fc9f465
AkshayPatil26 <akshpatil26@gmail.com>
2017-06-08 16:02:45 -0400

    Lec 79 webmaps with python and folium
    Dynamic popups on folium map markers executed

---
+++ maptest6.py
@@ -0,0 +1,31 @@
+"""
+adding multiple DYNAMIC POPUP markers from data file lec 78
+"""
+
+
+import pandas
+import folium
+
+
+data = pandas.read_csv("Volcanoes.txt")#reads csvfile in the folder
+
+#conversion of LAT,LON column into list from the data
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+#map.add_child(folium.Marker(location=[80.01,-100.01],popup="This is test marker", icon=folium.Icon(color='green')))
+
+map = folium.Map(location=[44,-120], zoom_start=4) #creates map
+
+#creates map object for marker
+fg = folium.FeatureGroup(name="My Map")
+
+#for loop for multiplecoordinates as list
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el)+" m", icon=folium.Icon(color='green')))
+    #popup accepts only string characters
+    map.add_child(fg)
+
+
+map.save("maptest6.html")
