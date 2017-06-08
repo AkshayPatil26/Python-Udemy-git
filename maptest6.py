f2c45ba9b52cbc262007db4bffb37bb78762cb11
AkshayPatil26 <akshpatil26@gmail.com>
2017-06-08 16:27:38 -0400

    Lec 80 Color based point markers
    Added a function to calculate the heightof the elevation and return the
    color value.
    The value is then passed through
    icon=folium.Icon(color=color_producer(el)

--- maptest6.py
+++ maptest6.py
@@ -16,6 +16,14 @@ elev = list(data["ELEV"])
 
 #map.add_child(folium.Marker(location=[80.01,-100.01],popup="This is test marker", icon=folium.Icon(color='green')))
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[44,-120], zoom_start=4) #creates map
 
 #creates map object for marker
@@ -23,8 +31,9 @@ fg = folium.FeatureGroup(name="My Map")
 
 #for loop for multiplecoordinates as list
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el)+" m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el)+" m", icon=folium.Icon(color=color_producer(el))))
     #popup accepts only string characters
+
     map.add_child(fg)
 
 
